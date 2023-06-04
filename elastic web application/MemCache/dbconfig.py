c1cb18fa818a716e61db23ffc4d8592c96f136f7
elvafan <40036572+elvafan@users.noreply.github.com>
2023-06-03 23:07:07 -0400

    Add files via upload

---
+++ elastic web application/MemCache/dbconfig.py
@@ -0,0 +1,10 @@
+# Please change to your sql account
+db_config = {'user': 'root',
+             'password': 'ece1779pass',
+             'host': 'a2serverdb.cm3f3izncqqg.us-east-1.rds.amazonaws.com',
+             'database': 'serverdb'}
+
+# db_config = {'user': 'yaoyinji',
+#              'password': 'gurrand172',
+#              'host': '127.0.0.1',
+#              'database': 'serverdb'}
